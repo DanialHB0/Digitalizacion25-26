0ac446e0ea0bc7c4ec4f1d2fadcebb94738405fe
DanialHB0 <danialvheday@gmail.com>
2025-12-03 13:40:45 +0100

    EJSPT2(mitad)

--- A2/PT1.py
+++ A2/PT1.py
@@ -1,34 +1,34 @@
-# EJ1.1 crear llista
+# EJ1.1
 llista = [3, 7, 1, 9, 4]
 print("EJ1.1:", llista)
 
-# EJ1.2 accedir elements
+# EJ1.2
 noms = ["Anna", "Pau", "Maria", "Joan", "Laia"]
 print("EJ1.2 primer:", noms[0])
 print("EJ1.2 ultim:", noms[-1])
 
-# EJ1.3 afegir / eliminar
+# EJ1.3
 llista2 = [10, 20, 30, 40]
 llista2.append(50)
 del llista2[0]
 print("EJ1.3:", llista2)
 
-# EJ1.4 longitud
+# EJ1.4
 print("EJ1.4 len llista:", len(llista))
 print("EJ1.4 len noms:", len(noms))
 
-# EJ2 comptar aparicions
+# EJ2
 list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
 print("EJ2:", list_num.count(7))
 
-# EJ3 suma
+# EJ3
 list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
 print("EJ3:", sum(list_num))
 
-# EJ4 max min
+# EJ4
 print("EJ4 max:", max(list_num), "min:", min(list_num))
 
-# EJ5 duplicats
+# EJ5
 list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 print("EJ5.1 set:", list(set(list_num)))
 nova = []
@@ -37,21 +37,21 @@ for x in list_num:
         nova.append(x)
 print("EJ5.2 ordre:", nova)
 
-# EJ6 invertir
+# EJ6 
 list_num = [2, 7, 4, 5, 1, 8, 3, 9, 6, 10]
 print("EJ6:", list_num[::-1])
 
-# EJ7 concatenar
+# EJ7 
 list_num = [2, 7, 4, 5, 1]
 list_num2 = [8, 3, 9, 6, 10]
 print("EJ7:", list_num + list_num2)
 
-# EJ8 pertinença
+# EJ8 
 list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 num = int(input("EJ8 num? "))
 print("EJ8:", "si" if num in list_num else "no")
 
-# EJ9.1 min successius
+# EJ9.1 
 list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 ordenada = []
 copia = list_num[:]
@@ -61,7 +61,7 @@ while copia:
     copia.remove(m)
 print("EJ9.1:", ordenada)
 
-# EJ9.2 selection sort
+# EJ9.2 
 list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 for i in range(len(list_num)):
     mi = i
@@ -71,7 +71,7 @@ for i in range(len(list_num)):
     list_num[i], list_num[mi] = list_num[mi], list_num[i]
 print("EJ9.2:", list_num)
 
-# EJ9.3 insertion sort
+# EJ9.3 
 list_num = [2, 7, 4, 2, 1, 6, 3, 7, 6, 10]
 for i in range(1, len(list_num)):
     k = list_num[i]
--- A2/PT2.py
+++ A2/PT2.py
@@ -1,6 +1,37 @@
 # EJ1
-cotxe = {"marca": "Toyota", "model": "Corolla", "any": 2010}
+cotxe = {
+    "marca": "Toyota",
+    "model ": "Corolla",
+    "any": 2020
+}
+
 print(cotxe["marca"])
 
-# EJ2
 
+# EJ2
+el_meu_cotxe = {"marca": "Toyota", "model": "Corolla", "any": 2020}
+el_meu_cotxe["color"] = "vermell"
+del el_meu_cotxe["any"]
+print(el_meu_cotxe)
+
+# EJ3
+capitals = {"Espanya": "Madrid", "França": "París", "Itàlia": "Roma"}
+for pais, capital in capitals.items():
+    print(f"La capital de {pais} és {capital}.")
+
+# EJ4
+pais_buscat = input("Digues un país: ")
+if pais_buscat in capitals:
+    print(f"La capital de {pais_buscat} és {capitals[pais_buscat]}.")
+else:
+    print(f"No conec la capital d'aquest país, ho sento!")
+
+# EJ5
+paraula = input("Escriu una paraula: ")
+comptador = {}
+for lletra in paraula:
+    if lletra in comptador:
+        comptador[lletra] += 1
+    else:
+        comptador[lletra] = 1
+print(comptador)
